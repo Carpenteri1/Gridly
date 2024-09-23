129521b78984364d7e98ac3f2f0ffab64965d0a9
Niclas Timle <niclastimle@Niclass-MacBook-Air.local>
2024-09-23 20:29:14 +0200

    Setup layout

---
+++ src/Components/BasicComponent/basic.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'basic-component',
+    standalone: true,
+    templateUrl: './basic.component.html',
+    styleUrl: './basic.component.css',
+  })
+export class BasicComponent{
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { BasicComponent } from '../Components/BasicComponent/basic.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  imports: [BasicComponent],
 })
 export class AppComponent {
   title = 'Gridly';
