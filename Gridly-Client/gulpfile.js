b446b9f0da06e91943e518368a0ef62221d210e4
niclastimle <niclastimle@icloud.com>
2025-03-14 21:08:17 +0100

    changes to gulp file

--- Gridly-Client/gulpfile.js
+++ Gridly-Client/gulpfile.js
@@ -6,14 +6,21 @@ gulp.task('build-angular', function () {
   return gulp.src('./')
     .pipe(exec('ng build --output-path=dist'));
 });
-gulp.task("move-build", function () {
-  return gulp.src("./dist/browser/**")
-    .pipe(gulp.dest("../wwwroot/"));
+
+gulp.task("move-angular", function () {
+  return gulp.src(["./dist/browser/*","./dist/browser/**"],
+    {encoding: false},
+    {overwrite: true})
+    .pipe(gulp.dest("../wwwroot/"),gulp.dest("../wwwroot/media/"));
 });
 
-gulp.task("clean-build", function () {
-  return gulp.src(["./dist/","../wwwroot/index.csr.html"],{read: false})
-    .pipe(clean({force: true}));
+gulp.task("clean-angular", function () {
+  return gulp.src(["./dist/","../wwwroot/index.csr.html"],{allowEmpty: true})
+    .pipe(clean({force: true},));
 });
 
-gulp.task("build", gulp.series("build-angular", "move-build", "clean-build"));
+gulp.task("build-angular", gulp.series(
+  "build-angular",
+  "move-angular",
+  "clean-angular"
+));
