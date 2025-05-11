8500f2819412e209fc75e1ea93818aec0121ccfb
niclastimle <niclastimle@icloud.com>
2025-05-11 21:22:24 +0200

    make dropdown menu

--- Gridly-Client/src/app/components/header/header.component.ts
+++ Gridly-Client/src/app/components/header/header.component.ts
@@ -1,17 +1,18 @@
-import {Component, Injectable, OnInit} from "@angular/core";
-import {HandleComponent} from "../modals/handleComponent/handle.component";
-import {SharedService} from '../../Services/shared.service';
-import {CommonModule} from "@angular/common";
+import { Component, Injectable, OnInit, ViewEncapsulation } from "@angular/core";
+import { HandleComponent } from "../modals/handleComponent/handle.component";
+import { SharedService } from '../../Services/shared.service';
+import { CommonModule } from "@angular/common";
+import { MatFormField, MatLabel } from "@angular/material/input";
+import { MatOption } from "@angular/material/core";
+import { MatSelect } from "@angular/material/select";
 
 @Component({
   selector: 'header-component',
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true,
-  imports: [
-    HandleComponent,
-    CommonModule
-  ]
+  encapsulation: ViewEncapsulation.None,
+  imports: [HandleComponent, CommonModule, MatFormField, MatOption, MatSelect, MatLabel]
 })
 
 @Injectable({ providedIn: 'root' })
