435a85744333c3b7f71dfc92d337105fa3eb3395
niclastimle <niclastimle@icloud.com>
2025-10-17 17:38:51 +0200

    update gulp with net

--- Gridly-Client/gulpfile.js
+++ Gridly-Client/gulpfile.js
@@ -1,26 +1,59 @@
 var gulp = require("gulp");
 var exec = require("gulp-exec");
-var clean = require('gulp-clean');
+var clean = require("gulp-clean");
+var merge = require("merge-stream");
 
-gulp.task('ng-build', function () {
-  return gulp.src('./')
-    .pipe(exec('ng build --output-path=dist'));
+// ---- Angular ----
+gulp.task("ng-build", function () {
+  return gulp
+    .src(".", { allowEmpty: true })
+    .pipe(exec("ng build --output-path=dist"))
+    .pipe(exec.reporter());
 });
 
 gulp.task("ng-move-build", function () {
-  return gulp.src(["./dist/browser/*","./dist/browser/**"],
-    {encoding: false},
-    {overwrite: true})
-    .pipe(gulp.dest("../wwwroot/"),gulp.dest("../wwwroot/media/"));
+  return gulp
+    .src("./dist/browser/**/*", { allowEmpty: true })
+    .pipe(gulp.dest("../wwwroot/"));
 });
 
 gulp.task("ng-clean-build", function () {
-  return gulp.src(["./dist/","../wwwroot/index.csr.html"],{allowEmpty: true})
-    .pipe(clean({force: true},));
+  return gulp
+    .src(["./dist/", "../wwwroot/index.csr.html"], { allowEmpty: true })
+    .pipe(clean({ force: true }));
 });
 
-gulp.task("build-angular", gulp.series(
-  "ng-build",
-  "ng-move-build",
-  "ng-clean-build"
-));
+const DOTNET_CWD = "../";
+
+gulp.task("dotnet-restore", function () {
+  return gulp
+    .src(".", { allowEmpty: true })
+    .pipe(exec(`dotnet restore`))
+    .pipe(exec.reporter());
+});
+
+gulp.task("dotnet-build", function () {
+  return gulp
+    .src(".", { allowEmpty: true })
+    .pipe(exec(`dotnet build`))
+    .pipe(exec.reporter());
+});
+
+gulp.task("dotnet-run", function () {
+  return gulp
+    .src(".", { allowEmpty: true })
+    .pipe(exec(`dotnet run --no-build --project`))
+    .pipe(exec.reporter());
+});
+
+gulp.task(
+  "build-angular",
+  gulp.series(
+    "ng-build",
+    "ng-move-build",
+    "ng-clean-build",
+    "dotnet-restore",
+    "dotnet-build",
+    "dotnet-run"
+  )
+);
