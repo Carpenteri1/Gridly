7619996935ad414ad0d9295b0d0da2fca8de7928
niclastimle <niclastimle@icloud.com>
2025-10-18 11:02:52 +0200

    logic changes to gulp

--- Gridly-Client/src/app/Components/Header/header.component.ts
+++ Gridly-Client/src/app/Components/Header/header.component.ts
@@ -1,8 +1,5 @@
 import {Component, OnInit} from "@angular/core";
 import {CommonModule} from "@angular/common";
-import {MatFormField, MatLabel} from "@angular/material/input";
-import {MatOption} from "@angular/material/core";
-import {MatSelect} from "@angular/material/select";
 import {MatIcon} from "@angular/material/icon";
 import {TextStringsUtil} from "../../Constants/text.strings.util";
 import {UrlStringsUtil} from "../../Constants/url.strings.util";
@@ -17,7 +14,7 @@ import {VersionService} from "../../Services/version.services";
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
   standalone: true,
-  imports: [CommonModule, MatFormField, MatOption, MatSelect, MatLabel, MatIcon]
+  imports: [CommonModule, MatIcon]
 })
 export class HeaderComponent implements OnInit {
   protected readonly StringUtil = TextStringsUtil;
