98bb0877a371fbed44e7f474c8bf5d3e18867b56
Carpenteri1 <niclastimle@icloud.com>
2026-08-08 14:34:07 +0200

    edit gulp files and output

--- Gridly-Client/gulpfile.js
+++ Gridly-Client/gulpfile.js
@@ -1,15 +1,46 @@
 var gulp = require("gulp");
-var gulpexec = require("gulp-exec");
-var clean = require('gulp-clean');
 var { exec, spawn } = require('child_process');
 var ngProcess = null;
+const prom= require("fs/promises");
+const path = require("path");
+
 
 // ---- Angular ----
-gulp.task('ng-build', function () {
-  return gulp.src('./')
-    .pipe(gulpexec('ng build --output-path=dist'))
-    .pipe(gulpexec.reporter());
+function runCommand(command, args, options = {}) {
+  return new Promise((resolve, reject) => {
+    const child = spawn(command, args, {
+      cwd: __dirname,
+      stdio: "inherit",
+      shell: false,
+      ...options
+    });
+
+    child.on("error", reject);
+
+    child.on("close", (code) => {
+      if (code === 0) {
+        resolve();
+      } else {
+        reject(new Error(`${command} ${args.join(" ")} failed with code ${code}`));
+      }
+    });
+  });
+}
+
+gulp.task("ng-build", async function () {
+  const ng = process.platform === "win32"
+    ? path.resolve(__dirname, "node_modules/.bin/ng.cmd")
+    : path.resolve(__dirname, "node_modules/.bin/ng");
+
+  await runCommand(ng, ["build"]);
 });
+gulp.task("clean-build", async function () {
+  await Promise.all([
+    prom.rm("../wwwroot", {recursive: true, force: true}),
+    prom.rm("../bin", {recursive: true, force: true})
+  ]);
+});
+
 
 gulp.task("ng-serve", function (done) {
   console.log("🚀 Starting client server...");
@@ -19,14 +50,14 @@ gulp.task("ng-serve", function (done) {
     cwd: process.cwd()
   });
   MessageLoop("ng");
-      
+
   ngProcess.on('close', (code) => {
     if (code !== null) {
       console.log(`ng serve exited with code ${code}`);
       done();
     }
   });
-  
+
   ngProcess.on('error', (err) => {
     console.error('Failed to start ng serve:', err);
     done(err);
@@ -38,14 +69,14 @@ gulp.task("ng-serve", function (done) {
 
 gulp.task("ng-stop", function (done) {
   console.log("🛑 Stopping ng serve...");
-  
+
   if (ngProcess) {
     ngProcess.kill('SIGTERM');
     ngProcess = null;
   }
-  
+
   const isWindows = process.platform === 'win32';
-  
+
   if (isWindows) {
     exec('netstat -ano | findstr :4200', (error, stdout) => {
       if (stdout) {
@@ -70,81 +101,94 @@ gulp.task("ng-stop", function (done) {
   }
 });
 
-gulp.task("ng-move-build", function () {
-  return gulp.src(["./dist/browser/*","./dist/browser/**"],
-    {encoding: false},
-    {overwrite: true})
-    .pipe(gulp.dest("../wwwroot/"),gulp.dest("../wwwroot/media/"));
-});
+gulp.task("ng-move-build", async function () {
+  const source = path.resolve(__dirname, "../wwwroot/browser");
+  const target = path.resolve(__dirname, "../wwwroot");
 
-gulp.task("ng-clean-build", function () {
-  return gulp.src(["./dist/","../wwwroot/index.csr.html"],{allowEmpty: true})
-    .pipe(clean({force: true},));
+  await prom.cp(source, target, {
+    recursive: true,
+    force: true
+  });
+
+  await prom.rm(source, {
+    recursive: true,
+    force: true
+  });
 });
 
 // ---- dotnet ----
-gulp.task("dotnet-restore", function () {
-  return gulp
-    .src(".")
-    .pipe(gulpexec(`cd .. && dotnet restore`))
-    .pipe(gulpexec.reporter());
-});
-
-gulp.task("dotnet-build-debug", function () {
-  return gulp
-    .src(".")
-    .pipe(gulpexec(`cd .. && dotnet build --configuration Debug`))
-    .pipe(gulpexec.reporter());
+gulp.task("dotnet-build", async function () {
+  await runCommand("dotnet", ["build"], {
+    cwd: path.resolve(__dirname, "..")
+  });
 });
 
 gulp.task("dotnet-run", function () {
-  console.log("🚀 Starting .NET kestrel...");
-  gulp
-    .src(".")
-    .pipe(gulpexec(`cd .. && dotnet run`))
-    .pipe(gulpexec.reporter());
+  console.log("Starting .NET kestrel...");
+
+  const child = spawn("dotnet", ["run"], {
+    cwd: path.resolve(__dirname, ".."),
+    stdio: "inherit",
+    shell: false
+  });
+
+  child.on("error", (error) => {
+    console.error(error);
+  });
+
   MessageLoop("net");
-  return gulp;
+
+  return child;
 });
 
-function MessageLoop(session){
+function MessageLoop(session) {
   for (let i = 0; i <= 2; i++) {
     setTimeout(() => {
-      if (i === 0) console.log("✅ Doing stuff .. ⏳");
-      if (i === 1) console.log("✅ Success on stuff .. ✨");
-      if(session === "net"){
-        if (i === 2) console.log("✅ Up on http://localhost:7575 🚀");
+      if (i === 0) console.log("Doing stuff ..");
+      if (i === 1) console.log("Success on stuff ..");
+
+      if (session === "net" && i === 2) {
+        console.log("Up on http://localhost:7575");
       }
-      if(session === "ng"){
-        if (i === 2){
-          console.log("✅ Client up on http://localhost:4200/ - With live edit 🚀");
-        }
+
+      if (session === "ng" && i === 2) {
+        console.log("Client up on http://localhost:4200/ - With live edit");
       }
     }, i * 2000);
   }
 }
 
 gulp.task(
-  "build-net-debug",
+  "build-net",
   gulp.series(
-    "dotnet-restore",
-    "dotnet-build-debug"
+    "clean-build",
+    "dotnet-build"
   )
 );
 
 gulp.task(
-  "build-angular-net-debug",
+  "build-angular",
   gulp.series(
+    "clean-build",
+    "ng-build",
+    "ng-move-build"
+  )
+);
+
+gulp.task(
+  "run-angular",
+  gulp.series(
+    "clean-build",
+    "ng-serve",
+  )
+);
+
+gulp.task(
+  "run-net-angular",
+  gulp.series(
+    "clean-build",
     "ng-build",
     "ng-move-build",
-    "ng-clean-build",
-    "dotnet-build-debug"
-  )
-);
-
-gulp.task(
-  "build-angular-debug",
-  gulp.series(
-    "ng-serve",
+    "dotnet-run"
   )
 );
